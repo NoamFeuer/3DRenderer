c92a8a748ec0a7261b7b39645e4917ef72e5450a
Noam Feuer <pleasant.fir3@gmail.com>
2026-05-26 23:02:03 +0200

    Triangles in the cube now dont render if they're behind other triangles.

--- src/rendering/Renderer.cpp
+++ src/rendering/Renderer.cpp
@@ -14,12 +14,26 @@ Vect3 toScreenSpace(const Vect3& v, int width, int height) {
     );
 }
 
-void drawMesh(SDL_Renderer* renderer, const Mesh& mesh, const Mat4& proj, const Mat4& transform, const int w, const int h) {
+void drawMesh(SDL_Renderer* renderer, const Mesh& mesh, const Mat4& proj, const Mat4& transform, int width, int height) {
     for (auto& tri : mesh.triangles) {
-        Vect3 v1 = toScreenSpace(proj * (transform * tri.vertices[0]), w, h);
-        Vect3 v2 = toScreenSpace(proj * (transform * tri.vertices[1]), w, h);
-        Vect3 v3 = toScreenSpace(proj * (transform * tri.vertices[2]), w, h);
+        // transform vertices
+        Vect3 v1 = transform * tri.vertices[0];
+        Vect3 v2 = transform * tri.vertices[1];
+        Vect3 v3 = transform * tri.vertices[2];
 
-        drawTriangle(renderer, v1, v2, v3);
+        // calculate normal
+        Vect3 edge1 = v2 - v1;
+        Vect3 edge2 = v3 - v1;
+        Vect3 normal = edge1.cross(edge2);
+
+        // backface culling: skip if facing away from camera
+        if (normal.z >= 0) continue;
+
+        // project to screen space
+        Vect3 p1 = toScreenSpace(proj * v1, width, height);
+        Vect3 p2 = toScreenSpace(proj * v2, width, height);
+        Vect3 p3 = toScreenSpace(proj * v3, width, height);
+
+        drawTriangle(renderer, p1, p2, p3);
     }
 }
